6d2abda76f80e11fcd5298b681b77137b2f09ccc
nck.974 <nck.974@gmail.com>
2024-02-12 08:56:06 +0100

    Harmonize arrows

--- src/app/shared/components/go-back-button/go-back-button.component.ts
+++ src/app/shared/components/go-back-button/go-back-button.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
+import { NextButtonComponent } from '../next-button/next-button.component';
 
 @Component({
   selector: 'app-go-back-button',
   standalone: true,
-  imports: [],
+  imports: [
+    NextButtonComponent
+  ],
   templateUrl: './go-back-button.component.html',
   styleUrl: './go-back-button.component.scss'
 })
